3a457a50d9b81b77850698b68ff4b370300e8b5a
ehab <ehab.mohamed.nasr@gmail.com>
2016-08-06 11:48:56 +0200

    Starting Version 2

--- LockToggle_pushButton.py
+++ LockToggle_pushButton.py
@@ -7,12 +7,12 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 def checkInput():
-    if GPIO.input(24):
-        return True
-    else:
-        time.sleep(0.5)
-        return GPIO.input(24)
-    return
+    return GPIO.input(24)
+#        return True
+#    else:
+#        time.sleep(0.5)
+#        return GPIO.input(24)
+#    return
 
 
 def toggleState():
